abfd208f761bb83ff43bb823c7e6aa157545bc2c
binaykumar19 <binaykumar19@gmail.com>
2019-07-11 22:25:18 -0400

    added Remaining logic

---
+++ restClient/src/logic/ObjectSerialization.java
@@ -0,0 +1,71 @@
+package logic;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Order;
+
+public class ObjectSerialization {
+	
+	public static void serializeObjects(List<Order> orders){
+		try {
+			String fileName = "Get_Response_"+System.currentTimeMillis()+".txt";
+			FileOutputStream f = new FileOutputStream(new File(fileName));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+
+			// Write objects to file
+			for(Order order : orders) {
+				o.writeObject(order);
+			}
+			
+			o.close();
+			f.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} 
+	}
+	
+	public static List<Order> deserializeObjects(String fileName){
+		List<Order> orders = new ArrayList<>();
+		try {
+			
+			FileInputStream fi = new FileInputStream(new File(fileName));
+			ObjectInputStream oi = new ObjectInputStream(fi);
+
+			boolean cont = true;
+			while(cont){
+				Order order = (Order) oi.readObject();
+			      if(order != null)
+			         orders.add(order);
+			      else
+			         cont = false;
+			   }
+			
+			oi.close();
+			fi.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}catch (EOFException e) {			
+			System.out.println(e.getMessage());
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return orders;
+	}
+
+}
